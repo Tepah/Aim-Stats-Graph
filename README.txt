1ecb2dd31f491c525925933dd93979e3504cffc5
Pete Potipitak <TepahT@gmail.com>
2020-07-12 19:26:53 -0700

    Loading and Dumping Json works
    # working with jsons as really long dictionaries.

--- README.txt
+++ README.txt
@@ -2,9 +2,10 @@ A stat tracker for different modes in Kovaak's
 
 Plan:
 # Input phase
-    # During input have user put in training type
+    # During input have user put in training type or select from already made input types
     # Have user put in scores
-    # automatically put in current date
+    # automatically put in current data
+    
 # Output phase
     # Ask the user if they want to see a full graph of the whole history of the specific training
     # Show the graph for the specific training type
--- stat_tracker.py
+++ stat_tracker.py
@@ -8,6 +8,24 @@ try:
     with open(filename) as f:
         full_data = json.load(f)
 except FileNotFoundError:
-    full_data = {}
+    # creates the data file if none is found.
+    full_data = {'modes':[]}
     with open(filename, 'w') as f:
         json.dump(full_data, f)
+
+if full_data['modes']:
+    # reads the modes that have already been input and lets the user choose.
+    print("Select mode you want to add to: ")
+    for value, mode in enumerate(full_data['modes']):
+        print(f"{value} {mode['type']}")
+    print(f"{value+1} add new mode")
+    answer =  input()
+else:
+    add_new_mode()
+
+with open(filename, 'w') as f:
+    json.dump(full_data, f)
+
+def add_new_mode():
+    answer = input("Add the mode that you want to track: \n")
+    full_data['modes'].append({'type':answer})
