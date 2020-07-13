2e483d1be786f768d32ba9c1ca9077f3590715cc
Pete Potipitak <TepahT@gmail.com>
2020-07-13 12:03:15 -0700

    Working Functions
    # Didn't work earlier

--- stat_tracker.py
+++ stat_tracker.py
@@ -3,13 +3,12 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-def add_new_mode(all_modes, new_mode):
+def add_new_mode(all_modes):
     answer = input("Add the mode that you want to track: \n")
     all_modes.append({'name':answer})
-    new_mode = True
-    return full_data['modes'][-1]
+    return all_modes[-1]
 
-def choose_mode(all_modes, new_mode):
+def choose_mode(all_modes):
     """
     Makes the user choose a choice an option from all the modes.
     Args:
@@ -25,11 +24,12 @@ def choose_mode(all_modes, new_mode):
                 Please choose from: "
         try:
             if answer == (value+1):
-                mode_data = add_new_mode(all_modes, new_mode)
-            elif answer > (value+1) and answer < 0:
+                add_new_mode(all_modes)
+                answer = -1
+            elif answer < (value+1) and answer >= 0:
                 for value, mode in enumerate(all_modes):
                     if answer == value:
-                        mode_data = mode[answer]
+                        mode_data = answer
             else: 
                 print(error)
                 continue
@@ -37,7 +37,7 @@ def choose_mode(all_modes, new_mode):
             print(error)
             continue
         break
-    return mode_data
+    return answer
 
 # read the data file into the system
 filename = 'data/aim_data.json'
@@ -54,13 +54,15 @@ except FileNotFoundError:
 new_mode = False
 if full_data['modes']:
 # reads the modes that have already been input and lets the user choose.
-    mode_data = choose_mode(full_data['modes'], new_mode)
+    amount_before = len(full_data['modes'])
+    index = choose_mode(full_data['modes'])
+    if amount_before < len(full_data['modes']):
+        new_mode = True
 else:
 # adds new mode and sets it as default
-    mode_data = add_new_mode(full_data['modes'], new_mode)
+    add_new_mode(full_data['modes'])
+    new_mode = True
 
-if new_mode:
-    print("new_mode changed")
 
 with open(filename, 'w') as f:
     json.dump(full_data, f)
